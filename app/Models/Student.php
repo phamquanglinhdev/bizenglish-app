532b1063c285ccccd3ad85f882fea2406cb36b99
phamquanglinhdev <phamquanglinhdev@gmail.com>
2022-11-23 17:26:01 +0700

    add

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -110,7 +110,7 @@ class Student extends Model
         if ($staff != null) {
             return $staff;
         } else {
-            return $this->originStaff();
+            return $this->originStaff()->first()->name;
         }
     }
 
