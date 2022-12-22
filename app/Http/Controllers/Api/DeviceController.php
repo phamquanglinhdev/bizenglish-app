8238c0cd14be5db64d560d1b3773c0e1860f9c13
phamquanglinhdev <phamquanglinhdev@gmail.com>
2022-12-22 12:38:41 +0700

    add

--- app/Http/Controllers/Api/DeviceController.php
+++ app/Http/Controllers/Api/DeviceController.php
@@ -13,15 +13,25 @@ class DeviceController extends Controller
         $user = $request->user();
         $token = $request->token ?? null;
         $platform = $request->platform ?? "android";
+
         if ($token == null) {
             return response()->json(null, 400);
         }
-        $data = [
-            'user_id' => $user->id,
-            'token' => $token,
-            'platform' => $platform,
-        ];
-        Device::create($data);
+        if (Device::where("token", $token)->count() == 0) {
+            $data = [
+                'user_id' => $user->id,
+                'token' => $token,
+                'platform' => $platform,
+            ];
+            Device::create($data);
+        } else {
+            $data = [
+                'user_id' => $user->id,
+                'platform' => $platform,
+            ];
+            Device::where('token', $token)->update($data);
+        }
+
         return response()->json(["message" => "Success"], 200);
     }
 }
