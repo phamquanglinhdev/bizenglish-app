c5c9376fb14278c4c483fa3cb585f78ae029fecf
phamquanglinhdev <phamquanglinhdev@gmail.com>
2022-12-24 09:40:56 +0700

    add log list

--- app/Http/Controllers/Api/LogApiController.php
+++ app/Http/Controllers/Api/LogApiController.php
@@ -96,7 +96,7 @@ class LogApiController extends Controller
             'attachments' => json_encode($request->attachments) ?? null,
         ];
         try {
-            return $data["attachments"][0];
+            return var_dump($data["attachments"]);
 //            Log::create($data);
 //            return \response()->json(["message" => "Thành công"], 200);
         } catch (\Exception $exception) {
