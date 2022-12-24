f9041f32bc2b497ae9c7352eec48c13d789711bc
phamquanglinhdev <phamquanglinhdev@gmail.com>
2022-12-24 09:55:10 +0700

    add log list

--- app/Http/Controllers/Api/LogApiController.php
+++ app/Http/Controllers/Api/LogApiController.php
@@ -91,7 +91,7 @@ class LogApiController extends Controller
             'log_salary' => $request->logSalary ?? null,
             'lesson' => $request->lesson ?? null,
             'information' => $request->information ?? null,
-            'teacher_video' => json_encode($request->video) ?? null,
+//            'teacher_video' => json_encode($request->video) ?? null,
             'status' => $request->status ?? null,
             'assessment' => $request->assessment ?? null,
 //            'attachments' => $request->attachments ?? null,
