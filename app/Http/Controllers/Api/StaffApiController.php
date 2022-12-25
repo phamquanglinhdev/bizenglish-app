20af5d89709d639f6d559f9dcf0643f7e9ea570b
phamquanglinhdev <phamquanglinhdev@gmail.com>
2022-12-25 17:41:27 +0700

    hi

--- app/Http/Controllers/Api/StaffApiController.php
+++ app/Http/Controllers/Api/StaffApiController.php
@@ -3,19 +3,24 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Staff;
 use Illuminate\Http\Request;
 
 class StaffApiController extends Controller
 {
     public function index(Request $request)
     {
-        //
+        $page = $request->page ?? 1;
+        $start = ($page - 1) * 10 + 1;
+        $staff = Staff::where("disable", 0)->skip($start)->take(10)->get(["code", "name", "job", "phone", "email"]);
+        return response()->json($staff);
     }
 
     public function show(Request $request)
     {
         //
     }
+
     public function store(Request $request)
     {
         //
