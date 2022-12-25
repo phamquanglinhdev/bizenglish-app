22b53b8f746d0e30fd241aef03426ab5750b2f0e
phamquanglinhdev <phamquanglinhdev@gmail.com>
2022-12-25 18:47:47 +0700

    new s

--- app/Http/Controllers/Api/StaffApiController.php
+++ app/Http/Controllers/Api/StaffApiController.php
@@ -44,11 +44,12 @@ class StaffApiController extends Controller
             'name' => $request->name ?? null,
         ];
         try {
-            $staff = Staff::create($data);
-            foreach ($request->students as $student) {
-                Student::find($student->id)->update(["staff_id" => $staff->id]);
-            }
-            return response()->json(["message" => "Thành công"], 200);
+            return $request->students;
+//            $staff = Staff::create($data);
+//            foreach ($request->students as $student) {
+//                Student::find($student->id)->update(["staff_id" => $staff->id]);
+//            }
+//            return response()->json(["message" => "Thành công"], 200);
         } catch (\Exception $exception) {
             return response()->json(["message" => "Thành công"], 400);
         }
