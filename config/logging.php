e0ca47072be65936c004c83401cf900298f4d182
phamquanglinhdev <phamquanglinhdev@gmail.com>
2022-12-30 19:35:14 +0700

    add dât

--- config/logging.php
+++ config/logging.php
@@ -53,12 +53,8 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single', 'bugsnag', 'slack'],
+            'channels' => ['single', 'slack'],
             'ignore_exceptions' => false,
-        ],
-        'bugsnag' => [
-            'driver' => 'bugsnag',
-            'level' => env('LOG_LEVEL', 'debug'),
         ],
         'single' => [
             'driver' => 'single',
