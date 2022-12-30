2f863dfdd8ada41fc3a4a80925fea4bf880ea8b4
phamquanglinhdev <phamquanglinhdev@gmail.com>
2022-12-30 18:36:09 +0700

    add dât

--- config/app.php
+++ config/app.php
@@ -180,6 +180,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Bugsnag\BugsnagLaravel\BugsnagServiceProvider::class,
 
 
         /*
