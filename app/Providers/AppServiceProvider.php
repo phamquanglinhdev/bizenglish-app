4f99c9369f1da99d89714e4a1e3cf1c9ca5af3a4
phamquanglinhdev <phamquanglinhdev@gmail.com>
2023-01-06 23:47:04 +0700

    add drive

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,8 +26,40 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
+    function getUserIP() {
+        $ipaddress = '';
+        if (isset($_SERVER['HTTP_CLIENT_IP']))
+            $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+        else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+            $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        else if(isset($_SERVER['HTTP_X_FORWARDED']))
+            $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+        else if(isset($_SERVER['HTTP_X_CLUSTER_CLIENT_IP']))
+            $ipaddress = $_SERVER['HTTP_X_CLUSTER_CLIENT_IP'];
+        else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+            $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+        else if(isset($_SERVER['HTTP_FORWARDED']))
+            $ipaddress = $_SERVER['HTTP_FORWARDED'];
+        else if(isset($_SERVER['REMOTE_ADDR']))
+            $ipaddress = $_SERVER['REMOTE_ADDR'];
+        else
+            $ipaddress = 'UNKNOWN';
+        return $ipaddress;
+    }
     public function boot()
     {
+
+        dd($_SERVER);
+        try {
+            $maintain = DB::table("settings")->where("name", "maintain")->first();
+            if ($maintain != null) {
+
+            } else {
+
+            }
+        } catch (\Exception $exception) {
+
+        }
         if (isset($_COOKIE["language"])) {
             app()->setLocale("en");
         } else {
--- resources/views/vendor/backpack/base/dashboard.blade.php
+++ resources/views/vendor/backpack/base/dashboard.blade.php
@@ -1,3 +1,4 @@
 @if(backpack_user()->type == -1)
     @include("admin-dashboard")
 @endif
+
